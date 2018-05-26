f4dbb69c14d4e5c1af8fca49a1ad8192a714fbdd
Felix Breuer <f.breuer94@gmail.com>
2018-05-26 12:41:50 +0200

    webpack mode fix

--- webpack.config.js
+++ webpack.config.js
@@ -3,15 +3,15 @@ var path = require('path');
 var dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    mode: 'development',
+    mode: dev ? 'development' : 'production',
 
     devtool: dev ? 'inline-source-map' : 'none',
     context: __dirname,
     entry: dev
         ? [
-              'webpack-hot-middleware/client',
-              path.resolve(__dirname, 'client/index.js')
-          ]
+            'webpack-hot-middleware/client',
+            path.resolve(__dirname, 'client/index.js')
+        ]
         : [path.resolve(__dirname, 'client/index.js')],
     resolve: {
         extensions: ['.js', '.jsx']
@@ -33,8 +33,8 @@ module.exports = {
     },
     plugins: dev
         ? [
-              new webpack.NamedModulesPlugin(),
-              new webpack.HotModuleReplacementPlugin()
-          ]
+            new webpack.NamedModulesPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        ]
         : []
 };
